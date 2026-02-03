28be06493eed84084cdf3bc6de0f3110bd87094d
owen <owen.blanchandin@gmail.com>
2026-02-03 18:35:33 +0000

    added main plus base luanch text

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Engine starting...\n" 
+              << "This is Engine Zero version: Undifined. \n" 
+              << "no updates for client side detected \n";
+    return 0;
+}
